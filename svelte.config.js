0ccb5666287f129e4f4be87e72c6700daa4f77af
Lukas Jørgensen <lukas.dinven@gmail.com>
2022-11-16 09:27:41 +0100

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html',
+			precompress: false,
+			strict: false,
+		}),
+		prerender: { entries: [] }
 	},
 	preprocess: [
     preprocess({
